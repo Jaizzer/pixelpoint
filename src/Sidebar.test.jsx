b5db64c05629e923dfc7781bd68c854d53c7cccd
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-12-30 07:23:04 +0800

    test: Use Memory Router to prevent context related errors and warnings

--- src/Sidebar.test.jsx
+++ src/Sidebar.test.jsx
@@ -1,10 +1,15 @@
 import { describe, it, expect } from 'vitest';
 import { getAllByRole, render, screen } from '@testing-library/react';
 import Sidebar from './Sidebar';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('Sidebar component', () => {
 	it('contains all navigation items of the page', () => {
-		render(<Sidebar />);
+		render(
+			<MemoryRouter>
+				<Sidebar />
+			</MemoryRouter>
+		);
 		const sidebar = screen.getByRole('navigation');
 		const navigationItems = getAllByRole(sidebar, 'listitem');
 		expect(navigationItems.length).toEqual(6);
