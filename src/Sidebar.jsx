a8f3401c474eb4b63baa18424417627e51229ed8
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-12-30 07:12:54 +0800

    feat: Use Link component for to prevent browser reload when going to different links

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,12 +1,11 @@
-
-
+import { Link } from 'react-router-dom';
 export default function Sidebar() {
     return (
         <nav>
             <ul>
                 <li>Logo</li>
                 <li>User</li>
-                <li>Home</li>
+                <li><Link to='/home'>Home</Link></li>
                 <li>Shop</li>
                 <li>Account</li>
                 <li>About</li>
