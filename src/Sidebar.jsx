61f5338ceb9bb53b738a7903460e1a0ff324a508
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-01-07 06:30:55 +0800

    feat: Add link to the remaining sidebar items

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,15 +1,27 @@
 import { Link } from 'react-router-dom';
 export default function Sidebar() {
-    return (
-        <nav>
-            <ul>
-                <li>Logo</li>
-                <li>User</li>
-                <li><Link to='/'>Home</Link></li>
-                <li>Shop</li>
-                <li>Account</li>
-                <li>About</li>
-            </ul>
-        </nav>
-    )
+	return (
+		<nav>
+			<ul>
+				<li>
+					<Link to="/">Logo</Link>
+				</li>
+				<li>
+					<Link to="/user">User</Link>
+				</li>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="/shop">Shop</Link>
+				</li>
+				<li>
+					<Link to="/account">Account</Link>
+				</li>
+				<li>
+					<Link to="/about">About</Link>
+				</li>
+			</ul>
+		</nav>
+	);
 }
