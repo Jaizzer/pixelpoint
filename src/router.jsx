fb5033248e9c9466afe3f818e91a6e465ea83071
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-01-07 07:14:43 +0800

    feat: Add error element to handle non-existent paths

--- src/router.jsx
+++ src/router.jsx
@@ -1,9 +1,11 @@
 import App from './App';
+import Error from './Error';
 import { createBrowserRouter } from 'react-router-dom';
 
 const routes = [
 	{
 		path: '/',
+		errorElement: <Error />,
 		element: <App />,
 	},
 	{
