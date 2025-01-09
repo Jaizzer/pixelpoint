2a8447bc29874cc6abedc2c255cf2a43664d7e58
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-01-10 07:59:02 +0800

    feat: Add title attribute to cart for accessing the element during testing

--- src/CartIcon.jsx
+++ src/CartIcon.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 function CartIcon({ cartContentCount }) {
 	return (
-		<button className="cart">
+		<button className="cart" title="cart">
 			<div className="cartIcon">Cart</div>
 			{cartContentCount === 0 ? null : <div className="cartContentCount">{cartContentCount}</div>}
 		</button>
