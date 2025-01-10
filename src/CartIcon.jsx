2f35e98a87e92de92e727e4c14ad3f3e4e0f501c
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-01-10 16:13:42 +0800

    feat: Add title attribute to the cart content count for testing purposes

--- src/CartIcon.jsx
+++ src/CartIcon.jsx
@@ -4,7 +4,11 @@ function CartIcon({ cartContentCount }) {
 	return (
 		<button className="cart" title="cart-icon">
 			<div className="cartIcon">Cart</div>
-			{cartContentCount === 0 ? null : <div className="cartContentCount">{cartContentCount}</div>}
+			{cartContentCount === 0 ? null : (
+				<div className="cartContentCount" title="cart-content-count">
+					{cartContentCount}
+				</div>
+			)}
 		</button>
 	);
 }
