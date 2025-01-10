0d9ed3695a865066613c135cefb01e3e49c38c59
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-01-10 08:22:12 +0800

    feat: Modify title attribute to prevent clash with the Cart page title attribute

--- src/CartIcon.jsx
+++ src/CartIcon.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 function CartIcon({ cartContentCount }) {
 	return (
-		<button className="cart" title="cart">
+		<button className="cart" title="cart-icon">
 			<div className="cartIcon">Cart</div>
 			{cartContentCount === 0 ? null : <div className="cartContentCount">{cartContentCount}</div>}
 		</button>
