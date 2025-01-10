260d21c98b7b805d605a9add767cf030eaacf57c
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-01-10 08:23:09 +0800

    feat: Use memory router to remove error associated with adding the Link router component

--- src/TopBar.test.jsx
+++ src/TopBar.test.jsx
@@ -1,26 +1,39 @@
 import { describe, it, expect, vi } from 'vitest';
 import { screen, render } from '@testing-library/react';
 import TopBar from './TopBar';
+import { MemoryRouter } from 'react-router-dom';
 
 vi.mock('./CartIcon', () => ({
-	default: ({cartContentCount}) => <button>{cartContentCount}</button>,
+	default: ({ cartContentCount }) => <button>{cartContentCount}</button>,
 }));
 
 describe('TopBar component', () => {
 	it('has a search bar', () => {
-		render(<TopBar />);
+		render(
+			<MemoryRouter>
+				<TopBar />
+			</MemoryRouter>
+		);
 		const searchBar = screen.queryByRole('textbox');
 		expect(searchBar).not.toBeNull();
 	});
 
 	it('has 3 buttons for notification, shopping, cart', () => {
-		render(<TopBar />);
+		render(
+			<MemoryRouter>
+				<TopBar />
+			</MemoryRouter>
+		);
 		const buttons = screen.queryAllByRole('button');
 		expect(buttons.length).toEqual(3);
 	});
 
 	it('passes the right order count to the Cart icon', () => {
-		render(<TopBar cartContentCount={6} />);
+		render(
+			<MemoryRouter>
+				<TopBar cartContentCount={6} />
+			</MemoryRouter>
+		);
 		const cart = screen.queryByText('6');
 		screen.debug();
 		expect(cart).not.toBeNull();
