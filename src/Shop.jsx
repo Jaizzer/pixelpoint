10657071aa275a1addf87e23efb1a3f0b92e96c4
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-01-14 22:23:53 +0800

    feat: Integrate the Dropdown filter with the Shop component

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,25 +1,84 @@
 import ProductCard from './ProductCard';
 import PropTypes from 'prop-types';
+import DropdownFilter from './DropdownFilter';
+import { useEffect, useState } from 'react';
+
 function Shop({ products, error, loading, onAddItemToCart }) {
+	const [genreFilters, setGenreFilters] = useState([]);
+
+	useEffect(() => {
+		// Collect every possible distinct genres from the products
+		if (products) {
+			let updatedGenreFilters = [
+				...new Set(
+					products.map((product) => {
+						return product.genre;
+					})
+				),
+			].map((genreFilter) => {
+				return { name: genreFilter, isChecked: false };
+			});
+			setGenreFilters(updatedGenreFilters);
+		}
+	}, [products]);
+
 	let productCards;
 	if (products) {
-		productCards = products.map((product) => {
-			return (
-				<ProductCard
-					key={product.productId}
-					imageLink={product.imageLink}
-					productName={product.productName}
-					productPrice={product.productPrice}
-					onAddItemToCart={onAddItemToCart}
-					productCartQuantity={product.productCartQuantity}
-					productId={product.productId}
-				/>
-			);
+		// Get all currently checked genre filter items to be used for filtering the products to be rendered
+		let checkedGenreFilters;
+		const isThereAtleastOneGenreFilterChecked = genreFilters.filter((genreFilter) => genreFilter.isChecked).length === 0;
+		if (isThereAtleastOneGenreFilterChecked) {
+			// Get all the checked genre filter items
+			checkedGenreFilters = genreFilters.map((genreFilter) => genreFilter.name);
+		} else {
+			// Apply all filters (to render all items) if there is currently no checked genre filter
+			checkedGenreFilters = genreFilters.filter((genreFilter) => genreFilter.isChecked).map((genreFilter) => genreFilter.name);
+		}
+
+		// Filter the products to be rendered by genre
+		const filteredProducts = products.filter((product) => checkedGenreFilters.includes(product.genre));
+		productCards = filteredProducts.map((product) => {
+			if (product)
+				return (
+					<ProductCard
+						key={product.productId}
+						imageLink={product.imageLink}
+						productName={product.productName}
+						productPrice={product.productPrice}
+						onAddItemToCart={onAddItemToCart}
+						productCartQuantity={product.productCartQuantity}
+						productId={product.productId}
+					/>
+				);
 		});
 	}
 
 	return (
 		<div title="shop">
+			{
+				// Only render the genre dropdown filter if there are available genre filter
+				genreFilters.length > 0 ? (
+					<DropdownFilter
+						items={genreFilters}
+						title="Genre"
+						onDropdownItemClick={(clickedGenreFilterItemName) => {
+							// Save the Unchecked/Checked status of the genre filter item in the 'genreFilters state array
+							const updatedGenreFilters = genreFilters.map((genreFilter) =>
+								genreFilter.name === clickedGenreFilterItemName ? { ...genreFilter, isChecked: !genreFilter.isChecked } : genreFilter
+							);
+							setGenreFilters(updatedGenreFilters);
+						}}
+						onClearClick={() => {
+							// Save the unchecking of all genre filter items in the 'genreFilters state array
+							const updatedGenreFilters = genreFilters.map((genreFilter) => {
+								return { ...genreFilter, isChecked: false };
+							});
+							setGenreFilters(updatedGenreFilters);
+						}}
+					/>
+				) : null
+			}
+
 			{loading ? (
 				<div className="loading" title="loading">
 					Loading...
