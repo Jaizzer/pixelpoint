22c17ff49d82fb22676d8a5fbf50ec447b1276d8
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-01-14 22:25:38 +0800

    test: Add new test for the integration of the Dropdown feature and update previous test to prevent each one from breaking due to the newly added features

--- src/Shop.test.jsx
+++ src/Shop.test.jsx
@@ -1,14 +1,42 @@
 import { describe, it, expect, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import Shop from './Shop';
+import { userEvent } from '@testing-library/user-event';
 
 vi.mock('./ProductCard', () => ({
 	default: ({ imageLink, productName, productPrice }) => {
 		return (
 			<div>
-				<img src={imageLink} alt={productName} />
+				<img src={imageLink} alt={productName} role="image" />
 				<div>{productName}</div>
 				<div>{productPrice}</div>
+			</div>
+		);
+	},
+}));
+
+vi.mock('./DropdownFilter', () => ({
+	default: ({ items, title, onDropdownItemClick, numberOfShowLessItems, onClearClick }) => {
+		const dropdownItems = items.map((item) => {
+			return (
+				<div key={item.name} className="dropdownItem">
+					<input
+						type="checkbox"
+						id={item.name}
+						name={item.name}
+						onChange={() => {
+							onDropdownItemClick(item.name);
+						}}
+					/>
+					<label htmlFor={item.name}>{item.name}</label>
+				</div>
+			);
+		});
+
+		return (
+			<div>
+				<div className="filterButton">{title}</div>
+				<div className="dropdown">{dropdownItems}</div>
 			</div>
 		);
 	},
@@ -17,9 +45,9 @@ vi.mock('./ProductCard', () => ({
 describe('Shop component', () => {
 	it('converts all array of products into shopping cards', () => {
 		const products = [
-			{ imageLink: 'fakeLink', productName: 'product', productPrice: '$45', productId: '1' },
-			{ imageLink: 'fakeLink', productName: 'product', productPrice: '$55', productId: '2' },
-			{ imageLink: 'fakeLink', productName: 'product', productPrice: '$65', productId: '3' },
+			{ imageLink: 'fakeLink', productName: 'product', productPrice: '$45', productId: '1', genre: 'Action' },
+			{ imageLink: 'fakeLink', productName: 'product', productPrice: '$55', productId: '2', genre: 'Action' },
+			{ imageLink: 'fakeLink', productName: 'product', productPrice: '$65', productId: '3', genre: 'Mystery' },
 		];
 		render(<Shop products={products} />);
 
@@ -39,5 +67,27 @@ describe('Shop component', () => {
 		render(<Shop loading={false} products={null} error={true} />);
 		const errorMessage = screen.queryByTitle('error');
 		expect(errorMessage).not.toBeNull();
+	});
+
+	it('filters the product when a dropdown filter is clicked', async () => {
+		const user = userEvent.setup();
+		const products = [
+			{ imageLink: 'fakeLink', productName: 'product', productPrice: '$45', productId: '1', genre: 'Action' },
+			{ imageLink: 'fakeLink', productName: 'product', productPrice: '$55', productId: '2', genre: 'Action' },
+			{ imageLink: 'fakeLink', productName: 'product', productPrice: '$65', productId: '3', genre: 'Mystery' },
+			{ imageLink: 'fakeLink', productName: 'product', productPrice: '$65', productId: '4', genre: 'Adventure' },
+			{ imageLink: 'fakeLink', productName: 'product', productPrice: '$65', productId: '5', genre: 'Horror' },
+		];
+		render(<Shop loading={false} products={products} error={false} />);
+
+		// Filter by price
+		const actionDropdownFilter = screen.queryByText('Action');
+		await user.click(actionDropdownFilter);
+
+		// Check if the products that are 'Action' genre the only products that remained
+		// by checking if the number of images corresponds to the number of Action products provided which is 2
+		const isActionGenresRemained = screen.queryAllByRole('image');
+
+		expect(isActionGenresRemained.length).toEqual(2);
 	});
 });
