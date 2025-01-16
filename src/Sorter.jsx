d814cc07117f008cd703255cc1c1863074121573
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-01-17 00:26:05 +0800

    feat: Create initial implementation of the Sorter component

---
+++ src/Sorter.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+function Sorter({ onSortItemClick }) {
+	const [isEverySortOptionVisible, setIsEverySortOptionVisible] = useState(false);
+	const [selectedSortOption, setSelectedSortOption] = useState(null);
+
+	// Create the sort option items to be put in DOM
+	const sortOptions = ['Popularity', 'Release Date', 'Name: A to Z', 'Name: Z to A', 'Price: Low to High', 'Price: High to Low'];
+	const sortOptionsDOM = sortOptions.map((sortOption) => {
+		// Add distinction between selected and non-selected sort option
+		const className = sortOption === selectedSortOption ? 'sortOption selected' : 'sortOption';
+		return (
+			<div
+				key={sortOption}
+				className={className}
+				title="sort-option"
+				onClick={(e) => {
+					// Set the clicked sort option else unset the clicked sort option if its the currently selected sort option
+					let newlySelectedSortOption = e.target.textContent !== selectedSortOption ? e.target.textContent : null;
+					onSortItemClick(newlySelectedSortOption);
+					setSelectedSortOption(newlySelectedSortOption);
+				}}
+			>
+				{sortOption}
+			</div>
+		);
+	});
+
+	return (
+		<div className="sorter">
+			<button
+				className="sort"
+				onClick={() => {
+					setIsEverySortOptionVisible(!isEverySortOptionVisible);
+				}}
+			>
+				Sort
+			</button>
+			{isEverySortOptionVisible && <div className="sortOptions">{sortOptionsDOM}</div>}
+		</div>
+	);
+}
+
+Sorter.propTypes = {
+	onSortItemClick: PropTypes.func,
+};
+
+export default Sorter;
---
+++ src/Sorter.test.jsx
@@ -0,0 +1,28 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
+import Sorter from './Sorter';
+
+describe('Sorter Component', () => {
+	it('has a dropdown that can be expanded to see the sort options', async () => {
+		const user = userEvent.setup();
+		render(<Sorter />);
+		const sorterDropdown = screen.queryByText('Sort');
+		await user.click(sorterDropdown);
+
+		const expectedSortOptions = ['Popularity', 'Release Date', 'Name: A to Z', 'Name: Z to A', 'Price: Low to High', 'Price: High to Low'];
+		const sortOptions = screen.queryAllByTitle('sort-option').map((sortItem) => sortItem.textContent);
+		expect(sortOptions).toEqual(expectedSortOptions);
+	});
+
+	it('has a dropdown that can be collapsed to hide the sort options', async () => {
+		const user = userEvent.setup();
+		render(<Sorter />);
+		const sorterDropdown = screen.queryByText('Sort');
+		await user.click(sorterDropdown);
+		await user.click(sorterDropdown);
+
+		const sortOptions = screen.queryAllByTitle('sort-option').map((sortItem) => sortItem.textContent);
+		expect(sortOptions.length).toEqual(0);
+	});
+});
