00b56b046403a6276e4b883c06ae903dde64351b
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-01-21 18:14:50 +0800

    feat: Integrate the search component to the topbar for actual search functionality

--- src/TopBar.jsx
+++ src/TopBar.jsx
@@ -1,14 +1,12 @@
 import PropTypes from 'prop-types';
 import CartIcon from './CartIcon';
 import { Link } from 'react-router-dom';
+import Search from './Search';
 
 function TopBar({ cartContentCount }) {
 	return (
 		<div className="topBar">
-			<div className="searchBar">
-				<div className="searchLogoContainer"></div>
-				<input type="text" />
-			</div>
+			<Search />
 			<button className="notification">Notification</button>
 			<Link to="/cart">
 				<CartIcon cartContentCount={cartContentCount}></CartIcon>
