75da7aab320490ec07bd4a60440533c16c33e99f
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-01-21 18:14:00 +0800

    feat: Create search component to integrate search dropdown feature with the input box

---
+++ src/Search.jsx
@@ -0,0 +1,67 @@
+import { useState, useEffect } from 'react';
+import SearchDropdown from './SearchDropDown';
+
+function Search() {
+	const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+	const [inputValue, setInputValue] = useState('');
+	const [data, setData] = useState(null);
+	const [isLoading, setIsLoading] = useState(true);
+	const [error, setError] = useState(false);
+
+	// Wait for the user to stop typing before fetching the search result
+	useEffect(() => {
+		const delaySearch = setTimeout(() => {
+			getSearchResults(inputValue);
+		}, 400);
+
+		return () => {
+			// Cancel previous timers
+			clearTimeout(delaySearch);
+
+			// Display loading indicator whenever the user types again
+			setIsLoading(true);
+		};
+	}, [inputValue]);
+
+	async function getSearchResults(searchQuery) {
+		try {
+			// Get the search results
+			const response = await fetch(`https://api.rawg.io/api/games?key=99ef179fc1ee4d77a91ccee7e1bb59e6&search=${searchQuery}`);
+			const jsonData = await response.json();
+			const searchResults = jsonData.results.map((product) => ({
+				name: product.name,
+				id: product.id,
+				price: 89,
+				image: product.background_image,
+			}));
+			setData(searchResults);
+		} catch {
+			setError(true);
+		} finally {
+			setIsLoading(false);
+		}
+	}
+
+	return (
+		<div className="search">
+			<div className="searchBar">
+				<div className="searchLogoContainer"></div>
+				<input
+					type="text"
+					value={inputValue}
+					onChange={(e) => {
+						setInputValue(e.target.value);
+						if (e.target.value === '') {
+							setIsDropdownVisible(false);
+						} else {
+							setIsDropdownVisible(true);
+						}
+					}}
+				/>
+			</div>
+			{isDropdownVisible ? <SearchDropdown data={data} error={error} loading={isLoading} /> : null}
+		</div>
+	);
+}
+
+export default Search;
---
+++ src/Search.test.jsx
@@ -0,0 +1,55 @@
+import { describe, it, expect, vi, beforeEach } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Search from './Search';
+
+// Mock the Search Dropdown component
+vi.mock('./SearchDropdown', () => ({
+	default: ({ loading, data, error }) => {
+		let content;
+		if (loading) {
+			content = <div>Loading....</div>;
+		} else if (error) {
+			content = <div>Error</div>;
+		} else if (data) {
+			{
+				data.map((product) => <div key={product.id}>{product.name}</div>);
+			}
+		}
+		return (
+			<div className="searchDropdown" title="search-result-dropdown">
+				{content}
+			</div>
+		);
+	},
+}));
+
+describe('Search Component', () => {
+	beforeEach(() => {
+		// Mock the fetch function
+		window.fetch = vi.fn(() =>
+			Promise.resolve({
+				json: () =>
+					Promise.resolve([
+						{ image: 'imageLink', name: 'product1', price: 451, id: 1 },
+						{ image: 'imageLink', name: 'product2', price: 552, id: 2 },
+						{ image: 'imageLink', name: 'product3', price: 653, id: 3 },
+						{ image: 'imageLink', name: 'product4', price: 654, id: 4 },
+						{ image: 'imageLink', name: 'product5', price: 655, id: 5 },
+					]),
+			})
+		);
+	});
+
+	it('shows the search result dropdown when the user typed to the search bar', async () => {
+		const user = userEvent.setup();
+		render(<Search />);
+		const searchBar = screen.queryByRole('textbox');
+
+		await user.type(searchBar, 'product');
+		screen.debug();
+
+		const searchResultDropdown = screen.queryByTitle('search-result-dropdown');
+		expect(searchResultDropdown).not.toBeNull();
+	});
+});
