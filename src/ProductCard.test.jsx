1093428c259421d76ec5ab46bff3172d1c52b093
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-01-24 06:23:41 +0800

    feat: Wrap the ProductCard in Memory Router since Product Card uses Link

--- src/ProductCard.test.jsx
+++ src/ProductCard.test.jsx
@@ -1,6 +1,7 @@
 import { describe, it, expect, vi } from 'vitest';
 import { screen, render } from '@testing-library/react';
 import ProductCard from './ProductCard';
+import { MemoryRouter } from 'react-router-dom';
 
 vi.mock('./OrderCountController', () => ({
 	default: () => <button>Add to Cart</button>,
@@ -8,19 +9,31 @@ vi.mock('./OrderCountController', () => ({
 
 describe('Item Card Component', () => {
 	it('contains the product image', () => {
-		render(<ProductCard productName="Ramen" />);
+		render(
+			<MemoryRouter>
+				<ProductCard productName="Ramen" />
+			</MemoryRouter>
+		);
 		const image = screen.queryByAltText('Ramen');
 		expect(image).not.toBeNull();
 	});
 
 	it('contains the product name', () => {
-		render(<ProductCard productName="Ramen" />);
+		render(
+			<MemoryRouter>
+				<ProductCard productName="Ramen" />{' '}
+			</MemoryRouter>
+		);
 		const productName = screen.queryByText('Ramen');
 		expect(productName).not.toBeNull();
 	});
 
 	it('contains the product price', () => {
-		render(<ProductCard productPrice="$56.00" />);
+		render(
+			<MemoryRouter>
+				<ProductCard productPrice="$56.00" />{' '}
+			</MemoryRouter>
+		);
 		const productPrice = screen.queryByText(
 			/[$\xA2-\xA5\u058F\u060B\u09F2\u09F3\u09FB\u0AF1\u0BF9\u0E3F\u17DB\u20A0-\u20BD\uA838\uFDFC\uFE69\uFF04\uFFE0\uFFE1\uFFE5\uFFE6]/
 		);
@@ -28,7 +41,11 @@ describe('Item Card Component', () => {
 	});
 
 	it('contains the order count controller', () => {
-		render(<ProductCard />);
+		render(
+			<MemoryRouter>
+				<ProductCard />{' '}
+			</MemoryRouter>
+		);
 		const addToCartButton = screen.queryByText('Add to Cart');
 		expect(addToCartButton).not.toBeNull();
 	});
