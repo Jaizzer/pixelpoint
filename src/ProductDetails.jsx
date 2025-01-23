8c00c3052f71d9b834e0de37f57bcc0dc26f9698
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-01-23 11:16:54 +0800

    feat: Add product rating heading as a label

--- src/ProductDetails.jsx
+++ src/ProductDetails.jsx
@@ -14,7 +14,11 @@ function ProductDetails({ product, loading, error }) {
 			) : (
 				<div className="productDetails">
 					<h1 className="productTitle">{product.title}</h1>
-					<div className="productRating">{product.rating ? product.rating : 'No Rating'}</div>
+
+					<div className="productRating">
+						<div className="productRatingHeading">Rating</div>
+						<div className="productRatingContent">{product.rating ? product.rating : 'No Rating'}</div>
+					</div>
 					<div className="productDescriptionContainer">
 						<h2 className="productDescriptionHeading">Description</h2>
 						<div className="productDescriptionContent">{product.description ? product.description : 'No available description.'}</div>
