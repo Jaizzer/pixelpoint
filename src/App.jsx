2e26b9abb01eec39f7fcf289796ba1c45fa78219
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-01-31 11:54:12 +0800

    feat: Move the feature of fetching products into its own custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -7,53 +7,15 @@ import Error from './Error';
 import Cart from './Cart';
 import TopBar from './TopBar';
 import { useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
 import ProductDetails from './ProductDetails';
 import useFetchProduct from './useFetchProduct';
+import useFetchProducts from './useFetchProducts';
 
 export default function App() {
 	const { content } = useParams();
-	const [products, setProducts] = useState([]);
-	const [error, setError] = useState(null);
-	const [loading, setLoading] = useState(true);
 	const { id } = useParams();
 	const { product, isProductHaveError, isProductLoading } = useFetchProduct(id);
-	const [pageToRequestFromAPI, setPageToRequestFromAPI] = useState(1);
-
-	function getNewProducts() {
-		setPageToRequestFromAPI((prev) => prev + 1);
-	}
-
-	// Fetch the products using the RAWG GAMES API
-	useEffect(() => {
-		(async function () {
-			try {
-				const response = await fetch(
-					`https://api.rawg.io/api/games?key=99ef179fc1ee4d77a91ccee7e1bb59e6&page=${pageToRequestFromAPI}&page_size=100`
-				);
-				const jsonData = await response.json();
-				const modifiedProducts = jsonData.results.map((product) => {
-					return {
-						imageLink: product.background_image,
-						productName: product.name,
-						productPrice: Math.floor(Math.random() * (100 - 50) + 50),
-						productId: `${product.id}`,
-						genre: product.genres.map((genre) => genre.name),
-						platforms: product.platforms.map((platform) => platform.name),
-						unitsSold: Math.floor(Math.random() * 1000000),
-						releaseDate: product.released,
-						esrbRating: product.esrb_rating ? product.esrb_rating.name : 'Unrated',
-					};
-				});
-
-				setProducts(products.concat(modifiedProducts));
-			} catch (error) {
-				setError(error ? true : false);
-			} finally {
-				setLoading(false);
-			}
-		})();
-	}, [pageToRequestFromAPI]);
+	const [products, isProductsHaveError, isProductsLoading, getNewProducts] = useFetchProducts();
 
 	return (
 		<>
@@ -65,7 +27,7 @@ export default function App() {
 				) : content === 'account' ? (
 					<Account />
 				) : content === 'shop' ? (
-					<Shop products={products} error={error} loading={loading} getNewProducts={getNewProducts} />
+					<Shop products={products} error={isProductsHaveError} loading={isProductsLoading} getNewProducts={getNewProducts} />
 				) : content === 'about' ? (
 					<About />
 				) : content === 'cart' ? (
---
+++ src/useFetchProducts.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect, useRef } from 'react';
+
+export default function useFetchProducts() {
+	const [pageToRequestFromAPI, setPageToRequestFromAPI] = useState(1);
+	const [products, setProducts] = useState([]);
+	const [isProductsLoading, setIsProductsLoading] = useState(true);
+	const [isProductsHaveError, setIsProductsHaveError] = useState(false);
+	const hasFetchingBeenApproved = useRef(true);
+
+	function getNewProducts() {
+		// Move to next page
+		setPageToRequestFromAPI((prev) => prev + 1);
+
+		// Allow fetching since new page was requested
+		hasFetchingBeenApproved.current = true;
+	}
+
+	useEffect(() => {
+		if (hasFetchingBeenApproved.current) {
+			// Prevent double fetch request caused by StrictMode
+			hasFetchingBeenApproved.current = false;
+			(async function () {
+				try {
+					const response = await fetch(
+						`https://api.rawg.io/api/games?key=99ef179fc1ee4d77a91ccee7e1bb59e6&page=${pageToRequestFromAPI}&page_size=100`
+					);
+					const jsonData = await response.json();
+					const modifiedProducts = jsonData.results.map((product) => {
+						return {
+							imageLink: product.background_image,
+							productName: product.name,
+							productPrice: Math.floor(Math.random() * (100 - 50) + 50),
+							productId: `${product.id}`,
+							genre: product.genres.map((genre) => genre.name),
+							platforms: product.platforms.map((platform) => platform.name),
+							unitsSold: Math.floor(Math.random() * 1000000),
+							releaseDate: product.released,
+							esrbRating: product.esrb_rating ? product.esrb_rating.name : 'Unrated',
+						};
+					});
+					// Add the newly requested products to current products array
+					setProducts((product) => {
+						return product.concat(modifiedProducts);
+					});
+				} catch (error) {
+					setIsProductsHaveError(error ? true : false);
+				} finally {
+					setIsProductsLoading(false);
+				}
+			})();
+		}
+	}, [pageToRequestFromAPI]);
+
+	return [products, isProductsHaveError, isProductsLoading, getNewProducts];
+}
