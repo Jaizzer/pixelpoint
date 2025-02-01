a22160cd0c7d4071adec4a99fb64295c27ee9575
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-02-01 12:46:11 +0800

    feat: Create initial styling for the product cards

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -1,17 +1,59 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Container = styled.div`
+	min-width: 300px;
+	max-width: 500px;
+	border-radius: 15px;
+	overflow: hidden;
+	color: white;
+	display: grid;
+	grid-template-rows: 1fr;
+	grid-template-columns: 1fr;
+	justify-content: center;
+	transition: scale ease-in 150ms;
+	&:hover {
+		scale: 1.05;
+	}
+`;
+
+const ProductCardImage = styled.img`
+	height: 300px;
+	width: 100%;
+	grid-row: 1 / 3;
+	grid-column: 1 / 2;
+	object-fit: cover;
+`;
+
+const ProductInfo = styled.div`
+	padding: 15px;
+	grid-row: 2 / 3;
+	grid-column: 1 / 2;
+	backdrop-filter: blur(4px);
+	background-color: #00000085;
+	align-self: end;
+	display: grid;
+	gap: 5px;
+`;
+
+const Text = styled.div`
+	font-family: 'Poppins';
+	font-weight: ${(props) => (props.emphasize ? 'bold' : 'normal')};
+	font-size: ${(props) => (props.emphasize ? '20px' : '15px')};
+`;
 
 function ProductCard({ imageLink, productName, productPrice, productId }) {
 	return (
-		<div className="ProductCard" title="product-card">
-			<Link to={`/gameDetails/${productId}`}>
-				<img src={imageLink} role="image" alt={productName} />
-				<div className="productName" data-testid="productName">
-					{productName}
-				</div>
-				<div className="price">{'$' + productPrice}</div>
-			</Link>
-		</div>
+		<Link to={`/gameDetails/${productId}`}>
+			<Container title="product-card">
+				<ProductCardImage src={imageLink} role="image" alt={productName}></ProductCardImage>
+				<ProductInfo>
+					<Text data-testid="productName">{productName}</Text>
+					<Text emphasize={true}>{'$' + productPrice}</Text>
+				</ProductInfo>
+			</Container>
+		</Link>
 	);
 }
 
