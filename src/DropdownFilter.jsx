e8a6dad253cb219d25549822f43c36b59c1ff52b
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-02-02 12:17:56 +0800

    feat: Handle the case of numberOfShowLessItems being greater than items.length through if condiiton instead of try catch block

--- src/DropdownFilter.jsx
+++ src/DropdownFilter.jsx
@@ -117,87 +117,79 @@ const FilterActions = styled.div`
 `;
 
 function DropdownFilter({ items, title, onDropdownItemClick, numberOfShowLessItems, onClearClick }) {
-	try {
-		// Throw error if the number of items that should be visible when the dropdown is 'showing less' is greater than or equal the number of items itself
-		if (items.length <= numberOfShowLessItems) {
-			throw new Error('The "numberOfShowLessItems" should be less than "items.length"');
+	// State variable that determines whether the dropdown is expanded or collapsed
+	const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+
+	// State variable that determines whether the dropdown is in 'Show less' or 'Show more' mode
+	const [isEveryDropdownItemsVisible, setIsEveryDropdownItemsVisible] = useState(false);
+
+	// Create the dropdown items
+	let dropdownItems = [];
+	if (isEveryDropdownItemsVisible || !numberOfShowLessItems || numberOfShowLessItems >= items.length) {
+		/* Render all dropdown items if the dropdown is in 'Show more' mode or if there is no numberOfShowLessItems provided  
+        or if the numberOfShowLessItems is greater than the original number of items (which is impossible) */
+		dropdownItems = items.map((item) => {
+			return (
+				<DropdownItem
+					key={`${item.name}-${item.isChecked}`}
+					name={item.name}
+					isCheckedInitialValue={item.isChecked}
+					onDropdownItemClick={onDropdownItemClick}
+				/>
+			);
+		});
+	} else {
+		// Limit the number of dropdown items to 'numberOfShowLessItems' if 'numberOfShowLessItems' was provided
+		for (let index = 0; index < numberOfShowLessItems; index++) {
+			let item = items[index];
+			dropdownItems.push(
+				<DropdownItem
+					key={`${item.name}-${item.isChecked}`}
+					name={item.name}
+					isCheckedInitialValue={item.isChecked}
+					onDropdownItemClick={onDropdownItemClick}
+				/>
+			);
 		}
-
-		// State variable that determines whether the dropdown is expanded or collapsed
-		const [isDropdownVisible, setIsDropdownVisible] = useState(false);
-
-		// State variable that determines whether the dropdown is in 'Show less' or 'Show more' mode
-		const [isEveryDropdownItemsVisible, setIsEveryDropdownItemsVisible] = useState(false);
-
-		// Create the dropdown items
-		let dropdownItems = [];
-		if (isEveryDropdownItemsVisible || !numberOfShowLessItems) {
-			// Render all dropdown items if the dropdown is in 'Show more' mode or if there is no numberOfShowLessItems provided
-			dropdownItems = items.map((item) => {
-				return (
-					<DropdownItem
-						key={`${item.name}-${item.isChecked}`}
-						name={item.name}
-						isCheckedInitialValue={item.isChecked}
-						onDropdownItemClick={onDropdownItemClick}
-					/>
-				);
-			});
-		} else {
-			// Limit the number of dropdown items to 'numberOfShowLessItems' if 'numberOfShowLessItems' was provided
-			for (let index = 0; index < numberOfShowLessItems; index++) {
-				let item = items[index];
-				dropdownItems.push(
-					<DropdownItem
-						key={`${item.name}-${item.isChecked}`}
-						name={item.name}
-						isCheckedInitialValue={item.isChecked}
-						onDropdownItemClick={onDropdownItemClick}
-					/>
-				);
-			}
-		}
-
-		return (
-			<DropdownFilterContainer>
-				<FilterButton
-					className="filterButton"
-					onClick={() => {
-						// Switch back to 'Show less' mode when expanding or collapsing the dropdown
-						setIsEveryDropdownItemsVisible(false);
-						setIsDropdownVisible(!isDropdownVisible);
-					}}
-				>
-					{title}
-				</FilterButton>
-				{isDropdownVisible && (
-					<PopOver>
-						{dropdownItems}
-						<FilterActions>
-							{
-								// Only show 'Show less' or 'Show more' button if there is a provided number of items to be shown in 'Show less' mode
-								numberOfShowLessItems ? (
-									<div
-										className="expand-collapse-toggler"
-										onClick={() => {
-											setIsEveryDropdownItemsVisible(!isEveryDropdownItemsVisible);
-										}}
-									>
-										{isEveryDropdownItemsVisible ? 'Show less' : 'Show more'}
-									</div>
-								) : null
-							}
-							<div className="clear" onClick={onClearClick}>
-								Clear
-							</div>
-						</FilterActions>
-					</PopOver>
-				)}
-			</DropdownFilterContainer>
-		);
-	} catch (error) {
-		throw new Error(error);
 	}
+
+	return (
+		<DropdownFilterContainer>
+			<FilterButton
+				className="filterButton"
+				onClick={() => {
+					// Switch back to 'Show less' mode when expanding or collapsing the dropdown
+					setIsEveryDropdownItemsVisible(false);
+					setIsDropdownVisible(!isDropdownVisible);
+				}}
+			>
+				{title}
+			</FilterButton>
+			{isDropdownVisible && (
+				<PopOver>
+					{dropdownItems}
+					<FilterActions>
+						{
+							// Only show 'Show less' or 'Show more' button if there is a provided number of items to be shown in 'Show less' mode
+							items.length > numberOfShowLessItems ? (
+								<div
+									className="expand-collapse-toggler"
+									onClick={() => {
+										setIsEveryDropdownItemsVisible(!isEveryDropdownItemsVisible);
+									}}
+								>
+									{isEveryDropdownItemsVisible ? 'Show less' : 'Show more'}
+								</div>
+							) : null
+						}
+						<div className="clear" onClick={onClearClick}>
+							Clear
+						</div>
+					</FilterActions>
+				</PopOver>
+			)}
+		</DropdownFilterContainer>
+	);
 }
 
 function DropdownItem({ name, isCheckedInitialValue, onDropdownItemClick }) {
