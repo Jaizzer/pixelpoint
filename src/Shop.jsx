279bb6760387abc8c5cd8aaf365b091679f38095
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-02-03 08:56:33 +0800

    feat: Set the initial number of visible sort options to 3

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -304,7 +304,7 @@ function Shop({ products, error, loading, getNewProducts }) {
 			</div>
 			{
 				<div className="otherTools">
-					<Sorter onSortItemClick={setSortCriteria} numberOfShowLessItems={3}/>
+					<Sorter onSortItemClick={setSortCriteria} numberOfShowLessItems={3} />
 				</div>
 			}
 			<div
