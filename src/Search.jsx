9eab7662cec27c772d7a63d33eecace2714e66ae
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-02-04 05:08:36 +0800

    feat: Put the search function inside the useEffect since its the only place whre it gets called

--- src/Search.jsx
+++ src/Search.jsx
@@ -72,11 +72,23 @@ function Search() {
 
 	// Wait for the user to stop typing before fetching the search result
 	useEffect(() => {
-		const delaySearch = setTimeout(() => {
-			const { error, loading, searchResults } = getSearchResults(inputValue);
-			setError(error);
-			setIsLoading(loading);
-			setData(searchResults);
+		const delaySearch = setTimeout(async () => {
+			try {
+				// Get the search results
+				const response = await fetch(`https://api.rawg.io/api/games?key=99ef179fc1ee4d77a91ccee7e1bb59e6&search=${inputValue}`);
+				const jsonData = await response.json();
+				let searchResults = jsonData.results.map((product) => ({
+					name: product.name,
+					id: product.id,
+					price: 89,
+					image: product.background_image,
+				}));
+				setData(searchResults);
+			} catch {
+				setError(true);
+			} finally {
+				setIsLoading(false);
+			}
 		}, 400);
 
 		return () => {
@@ -87,31 +99,6 @@ function Search() {
 			setIsLoading(true);
 		};
 	}, [inputValue]);
-
-	async function getSearchResults(searchQuery) {
-		let searchResults = null;
-		let error = false;
-		let loading = true;
-		try {
-			// Get the search results
-			const response = await fetch(`https://api.rawg.io/api/games?key=99ef179fc1ee4d77a91ccee7e1bb59e6&search=${searchQuery}`);
-			const jsonData = await response.json();
-
-			searchResults = jsonData.results.map((product) => ({
-				name: product.name,
-				id: product.id,
-				price: 89,
-				image: product.background_image,
-			}));
-			setData(searchResults);
-		} catch {
-			error = true;
-		} finally {
-			loading = false;
-		}
-
-		return { error, loading, searchResults };
-	}
 
 	return (
 		<SearchContainer>
