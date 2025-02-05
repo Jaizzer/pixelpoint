dc6231e7b69270afa66e0ff1c6e1de8c7d4f16b7
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-02-05 08:54:06 +0800

    feat: Add styling to the cart content cound indicator

--- src/CartIcon.jsx
+++ src/CartIcon.jsx
@@ -1,15 +1,40 @@
 import PropTypes from 'prop-types';
+import { CartIcon as Icon } from './Icons';
+import styled from 'styled-components';
+
+const Container = styled.div`
+	box-sizing: border-box;
+	margin: 0px;
+
+	position: relative;
+	background-color: transparent;
+	font-family: 'Poppins';
+	font-size: 20px;
+	stroke: white;
+`;
+
+const CountIndicator = styled.div`
+	box-sizing: border-box;
+	margin: 0px;
+
+	position: absolute;
+	top: -8px;
+	right: -12px;
+	width: 1.5em;
+	scale: 0.5;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border-radius: 100px;
+	background-color: #099ea6;
+`;
 
 function CartIcon({ cartContentCount }) {
 	return (
-		<button className="cart" title="cart-icon">
-			<div className="cartIcon">Cart</div>
-			{cartContentCount === 0 ? null : (
-				<div className="cartContentCount" title="cart-content-count">
-					{cartContentCount}
-				</div>
-			)}
-		</button>
+		<Container title="cart-icon">
+			<Icon />
+			{cartContentCount === 0 ? null : <CountIndicator title="cart-content-count">{cartContentCount}</CountIndicator>}
+		</Container>
 	);
 }
 
