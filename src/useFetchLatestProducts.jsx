bc03174baec48a6400afb39b3bd61026ad0a1782
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-02-07 15:56:57 +0800

    feat: Use the number of people who owns the product to be the number of units sold

--- src/useFetchLatestProducts.jsx
+++ src/useFetchLatestProducts.jsx
@@ -41,7 +41,7 @@ export default function useFetchLatestProducts() {
 							productId: `${product.id}`,
 							genre: product.genres.map((genre) => genre.name),
 							platforms: product.platforms.map((platform) => platform.platform.name),
-							unitsSold: Math.floor(Math.random() * 1000000),
+							unitsSold: product.added_by_status && product.added_by_status.owned ? product.added_by_status.owned : 0,
 							releaseDate: product.released,
 							esrbRating: product.esrb_rating ? product.esrb_rating.name : 'Unrated',
 						};
