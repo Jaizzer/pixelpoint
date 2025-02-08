566d2003d0e17f64587f6a34fe1793a3cefe9608
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-02-09 05:06:02 +0800

    ref: Update variable and property names

--- src/useFetchGame.jsx
+++ src/useFetchGame.jsx
@@ -5,7 +5,7 @@ import getPrice from './getPrice';
 function useFetchGame(id) {
 	const [game, setGame] = useState(null);
 	const [isGameLoading, setIsGameLoading] = useState(true);
-	const [isGameHaveError, setIsGameHaveErrors] = useState(false);
+	const [gameError, setGameError] = useState(null);
 
 	// Get the necessary details about the clicked game
 	useEffect(() => {
@@ -14,32 +14,37 @@ function useFetchGame(id) {
 		if (id !== undefined) {
 			(async function () {
 				try {
-					const detailsResponse = await fetch(`https://api.rawg.io/api/games/${id}?key=99ef179fc1ee4d77a91ccee7e1bb59e6`);
-					const jsonDataDetails = await detailsResponse.json();
-					const screenshotsResponse = await fetch(`https://api.rawg.io/api/games/${id}/screenshots?key=99ef179fc1ee4d77a91ccee7e1bb59e6`);
-					const jsonDataImages = await screenshotsResponse.json();
-					const modifiedGame = {
-						id: jsonDataDetails.id,
-						title: jsonDataDetails.name,
-						description: jsonDataDetails.description_raw,
-						rating: jsonDataDetails.rating,
-						price: getPrice(jsonDataDetails.id),
-						developers: jsonDataDetails.developers.map((developer) => developer.name),
-						genres: jsonDataDetails.genres.map((genre) => genre.name),
-						releaseDate: jsonDataDetails.released,
-						platforms: jsonDataDetails.platforms.map((platform) => platform.platform.name),
-						screenshots: [jsonDataDetails.background_image, ...jsonDataImages.results.map((result) => result.image)],
+					// Get the details about the game
+					let response = await fetch(`https://api.rawg.io/api/games/${id}?key=99ef179fc1ee4d77a91ccee7e1bb59e6`);
+					const gameDetails = await response.json();
+
+					// Get the screenshots of the game
+					response = await fetch(`https://api.rawg.io/api/games/${id}/screenshots?key=99ef179fc1ee4d77a91ccee7e1bb59e6`);
+					const gameScreenshots = await response.json();
+
+					const gameWithDistilledProperties = {
+						id: gameDetails.id,
+						title: gameDetails.name,
+						description: gameDetails.description_raw,
+						rating: gameDetails.rating,
+						price: getPrice(gameDetails.id),
+						developers: gameDetails.developers.map((developer) => developer.name),
+						genres: gameDetails.genres.map((genre) => genre.name),
+						releaseDate: gameDetails.released,
+						platforms: gameDetails.platforms.map((platform) => platform.platform.name),
+						screenshots: [gameDetails.background_image, ...gameScreenshots.results.map((result) => result.image)],
 					};
-					setGame(modifiedGame);
+
+					setGame(gameWithDistilledProperties);
 				} catch (error) {
-					setIsGameHaveErrors(error ? true : false);
+					setGameError(error);
 				} finally {
 					setIsGameLoading(false);
 				}
 			})();
 		}
 	}, [id]);
-	return [ game, isGameHaveError, isGameLoading ];
+	return [game, gameError, isGameLoading];
 }
 
 useFetchGame.propTypes = {
