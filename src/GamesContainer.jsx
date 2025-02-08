7cd93bb3aa867ae5af6af1e396bb426d6a3290ea
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-02-09 05:07:51 +0800

    feat: Remove cart quanitity since it is no longer being used

--- src/GamesContainer.jsx
+++ src/GamesContainer.jsx
@@ -2,9 +2,7 @@ import PropTypes from 'prop-types';
 import GameCard from './GameCard';
 
 function GamesContainer({ games }) {
-	const gameCards = games.map((game) => (
-		<GameCard key={game.id} image={game.image} title={game.title} price={game.price} quantity={game.productCartQuantity} id={game.productId} />
-	));
+	const gameCards = games.map((game) => <GameCard key={game.id} image={game.image} title={game.title} price={game.price} id={game.productId} />);
 
 	return <div className="gamesContainer">{gameCards}</div>;
 }
