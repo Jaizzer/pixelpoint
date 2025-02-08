ebbeace6a7e5fb0d16dc9c5139402f621369f328
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-02-09 05:14:13 +0800

    ref: Update property name

--- src/GamesContainer.jsx
+++ src/GamesContainer.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import GameCard from './GameCard';
 
 function GamesContainer({ games }) {
-	const gameCards = games.map((game) => <GameCard key={game.id} image={game.image} title={game.title} price={game.price} id={game.productId} />);
+	const gameCards = games.map((game) => <GameCard key={game.id} image={game.image} title={game.title} price={game.price} id={game.id} />);
 
 	return <div className="gamesContainer">{gameCards}</div>;
 }
