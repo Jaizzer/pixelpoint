62b1efb3a4c88497f099487d4781e937c4c8f76a
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-02-13 11:53:36 +0800

    style: Add whitespace

--- src/GameCard.jsx
+++ src/GameCard.jsx
@@ -63,6 +63,7 @@ function GameCard({ image, title, price, rating, quantitySold, id }) {
 		</Link>
 	);
 }
+
 function nFormatter(num) {
 	if (num >= 1000000000) {
 		return (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'G';
