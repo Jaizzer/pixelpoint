e3df4cf48a73f700c8869e1ed752fa94302cc2e2
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-02-14 12:09:44 +0800

    test: Remove mocks since the components are in the same file as FeaturedGames

--- src/FeaturedGames.test.jsx
+++ src/FeaturedGames.test.jsx
@@ -1,20 +1,8 @@
-import { describe, it, expect, vi } from 'vitest';
+import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import FeaturedGames from './FeaturedGames';
-
-vi.mock('./SelectedFeaturedGame', () => ({
-	default: ({ game }) => {
-		return <div title="selected-featured-game">{game.title}</div>;
-	},
-}));
-
-vi.mock('./UnselectedFeaturedGame', () => ({
-	default: ({ game }) => {
-		return <div title="unselected-featured-game">{game}</div>;
-	},
-}));
 
 describe('Featured Games Component', () => {
 	it('displays the Loading indicator while loading the games', () => {
