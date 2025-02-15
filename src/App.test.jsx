cc5ff2e3cb50bd274eedf753d812a87969b5edc5
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-02-15 10:13:42 +0800

    test: Check if clicking the filters trigger new request for either genre-specific or platform-specific games

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,6 +5,50 @@ import { userEvent } from '@testing-library/user-event';
 import App from './App';
 import { useState } from 'react';
 
+vi.mock('./GamesContainer', () => ({
+	default: ({ games }) => {
+		const gameCards = games.map((game) => {
+			return (
+				<div key={game.id}>
+					<img src={game.image} alt={game.title} role="image" />
+					<div title="game-title">{game.title}</div>
+					<div>{game.price}</div>
+					<div title="game-genre">{game.genres.join(', ')}</div>
+					<div title="game-platform">{game.platforms.join(', ')}</div>
+				</div>
+			);
+		});
+		return <div>{gameCards}</div>;
+	},
+}));
+
+vi.mock('./DropdownFilter', () => ({
+	default: ({ items, title, onDropdownItemClick }) => {
+		const dropdownItems = items.map((item) => {
+			return (
+				<div key={item.name} className="dropdownItem">
+					<input type="checkbox" id={item.name} name={item.name} />
+					<label
+						htmlFor={item.name}
+						onClick={() => {
+							onDropdownItemClick(item);
+						}}
+					>
+						{item.name}
+					</label>
+				</div>
+			);
+		});
+
+		return (
+			<div>
+				<div className="filterButton">{title}</div>
+				<div className="dropdown">{dropdownItems}</div>
+			</div>
+		);
+	},
+}));
+
 vi.mock('./useFetchGenres.jsx', () => ({
 	default: () => {
 		const genres = [
@@ -146,14 +190,121 @@ vi.mock('./useFetchGames.jsx', () => {
 				);
 			});
 
-			function getSpecificPlatforms() {
+			function getSpecificPlatforms(platformIDs) {
+				function getPlatformName(platformId) {
+					switch (platformId) {
+						case 1:
+							return 'PC';
+						case 2:
+							return 'PlayStation 5';
+						case 3:
+							return 'Xbox One';
+						case 4:
+							return 'PlayStation 4';
+						case 5:
+							return 'Xbox Series S/X';
+						case 6:
+							return 'Nintendo Switch';
+						case 7:
+							return 'iOS';
+						case 8:
+							return 'Android';
+						case 9:
+							return 'Nintendo 3DS';
+						case 10:
+							return 'Nintendo DS';
+						case 11:
+							return 'Nintendo DSi';
+						case 12:
+							return 'macOS';
+						case 13:
+							return 'Linux';
+						case 14:
+							return 'Xbox 360';
+						case 15:
+							return 'Xbox';
+						case 16:
+							return 'PlayStation 3';
+						case 17:
+							return 'PlayStation 2';
+						case 18:
+							return 'PlayStation';
+						case 19:
+							return 'PS Vita';
+						case 20:
+							return 'PSP';
+						case 21:
+							return 'Wii U';
+						case 22:
+							return 'Wii';
+						case 23:
+							return 'GameCube';
+						case 24:
+							return 'Nintendo 64';
+						case 25:
+							return 'Game Boy Advance';
+						case 26:
+							return 'Game Boy Color';
+						case 27:
+							return 'Game Boy';
+						case 28:
+							return 'SNES';
+						case 29:
+							return 'NES';
+						case 30:
+							return 'Classic Macintosh';
+						case 31:
+							return 'Apple II';
+						case 32:
+							return 'Commodore / Amiga';
+						case 33:
+							return 'Atari 7800';
+						case 34:
+							return 'Atari 5200';
+						case 35:
+							return 'Atari 2600';
+						case 36:
+							return 'Atari Flashback';
+						case 37:
+							return 'Atari 8-bit';
+						case 38:
+							return 'Atari ST';
+						case 39:
+							return 'Atari Lynx';
+						case 40:
+							return 'Atari XEGS';
+						case 41:
+							return 'Genesis';
+						case 42:
+							return 'SEGA Saturn';
+						case 43:
+							return 'SEGA CD';
+						case 44:
+							return 'SEGA 32X';
+						case 45:
+							return 'SEGA Master System';
+						case 46:
+							return 'Dreamcast';
+						case 47:
+							return '3DO';
+						case 48:
+							return 'Jaguar';
+						case 49:
+							return 'Game Gear';
+						case 50:
+							return 'Neo Geo';
+						default:
+							return 'Unknown Platform';
+					}
+				}
+
 				setGames([
 					{
 						title: 'Game 10',
 						price: 50,
 						id: '1',
-						genres: ['Genre 6'],
-						platforms: ['Platform 5', 'Platform 5', 'Platform 5'],
+						genres: ['Action'],
+						platforms: platformIDs.map((platformID) => getPlatformName(platformID)),
 						ownerCount: 300,
 						releaseDate: '2024-01-24',
 						esrbRating: ['Everyone'],
@@ -162,14 +313,59 @@ vi.mock('./useFetchGames.jsx', () => {
 				]);
 			}
 
-			function getSpecificGenres() {
+			function getSpecificGenres(genreIDs) {
+				function getGenreName(genreId) {
+					switch (genreId) {
+						case 1:
+							return 'Action';
+						case 2:
+							return 'Indie';
+						case 3:
+							return 'Adventure';
+						case 4:
+							return 'RPG';
+						case 5:
+							return 'Strategy';
+						case 6:
+							return 'Shooter';
+						case 7:
+							return 'Casual';
+						case 8:
+							return 'Simulation';
+						case 9:
+							return 'Puzzle';
+						case 10:
+							return 'Arcade';
+						case 11:
+							return 'Platformer';
+						case 12:
+							return 'Massively Multiplayer';
+						case 13:
+							return 'Racing';
+						case 14:
+							return 'Sports';
+						case 15:
+							return 'Fighting';
+						case 16:
+							return 'Family';
+						case 17:
+							return 'Board Games';
+						case 18:
+							return 'Educational';
+						case 19:
+							return 'Card';
+						default:
+							return 'Unknown';
+					}
+				}
+
 				setGames([
 					{
 						title: 'Game 10',
 						price: 50,
 						id: '1',
-						genres: ['Genre 12', 'Genre 9'],
-						platforms: ['Platform 1', 'Platform 2'],
+						genres: genreIDs.map((genreID) => getGenreName(genreID)),
+						platforms: ['PC'],
 						ownerCount: 300,
 						releaseDate: '2024-01-24',
 						esrbRating: ['Everyone'],
@@ -431,5 +627,57 @@ describe('App component', () => {
 
 			expect(isCartContentGone && isCartCountIndicatorGone).toBeTruthy();
 		});
+	});
+
+	it('provides the games that match the requested genres', async () => {
+		const user = userEvent.setup();
+		render(
+			<MemoryRouter initialEntries={['/']}>
+				<Routes>
+					<Route path="/" element={<App />} />
+					<Route path="/:pageToDisplay" element={<App />} />
+				</Routes>
+			</MemoryRouter>
+		);
+
+		// Go to shop
+		const shop = screen.queryByText('Shop');
+		await user.click(shop);
+
+		// Filter all 'Action' and 'Puzzle'
+		const actionDropdownFilter = screen.queryByText('Action');
+		const puzzleDropdownFilter = screen.queryByText('Puzzle');
+		const arcadeDropdownFilter = screen.queryByText('Arcade');
+		await user.click(actionDropdownFilter);
+		await user.click(puzzleDropdownFilter);
+		await user.click(arcadeDropdownFilter);
+
+		const gameGenres = screen.queryByText('Action, Puzzle, Arcade');
+		expect(gameGenres).not.toBeNull();
+	});
+
+	it('provides the games that match the requested platforms', async () => {
+		const user = userEvent.setup();
+		render(
+			<MemoryRouter initialEntries={['/']}>
+				<Routes>
+					<Route path="/" element={<App />} />
+					<Route path="/:pageToDisplay" element={<App />} />
+				</Routes>
+			</MemoryRouter>
+		);
+
+		// Go to shop
+		const shop = screen.queryByText('Shop');
+		await user.click(shop);
+
+		// Filter all 'PC' and 'PlayStation 5'
+		const pcDropdownFilter = screen.queryByText('PC');
+		const ps5DropdownFilter = screen.queryByText('PlayStation 5');
+		await user.click(pcDropdownFilter);
+		await user.click(ps5DropdownFilter);
+
+		const gamePlatforms = screen.queryByText('PC, PlayStation 5');
+		expect(gamePlatforms).not.toBeNull();
 	});
 });
