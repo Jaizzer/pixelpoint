0a5f78ba64481cb16d2589a5bc891d0581a07a21
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-02-16 07:21:11 +0800

    feat: Add content to error page

--- src/Error.jsx
+++ src/Error.jsx
@@ -1,7 +1,17 @@
+import astrobotPng from './assets/images/astrobot-2.png';
+
+import { Link } from 'react-router-dom';
 export default function Error() {
 	return (
 		<div className="errorPage" title="error">
-			The path does not exist
+			<div className="errorCard">
+				<img src={astrobotPng} className="error" />
+				<h1>404</h1>
+				<div className="message">Page not found</div>
+				<Link to="/shop">
+					<button>← Back To Shop</button>
+				</Link>
+			</div>
 		</div>
 	);
 }
