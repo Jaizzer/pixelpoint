c25f3f3928ff7c74a33e4bcd1a6c9710ef21b1cd
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-02-18 05:23:22 +0800

    test: Check if games container is calling the get new games function whenever Show More button is cliked

--- src/GamesContainer.test.jsx
+++ src/GamesContainer.test.jsx
@@ -1,5 +1,6 @@
 import { describe, it, expect, vi } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import GamesContainer from './GamesContainer.jsx';
 
 vi.mock('./GameCard', () => ({
@@ -52,5 +53,51 @@ describe('Games Container Component', () => {
 		render(<GamesContainer games={[]} isGamesLoading={true} error={new Error()} />);
 		const loadingMessage = screen.queryByTitle('loading');
 		expect(loadingMessage).not.toBeNull();
+	});
+
+	it('gets new games if the "Show More" button is clicked', async () => {
+		const user = userEvent.setup();
+		const getNewGamesMock = vi.fn();
+		const games = [
+			{
+				images: ['fakeLink'],
+				title: 'Game 1',
+				price: 45,
+				id: '1',
+				genres: ['Action', 'Adventure'],
+				platforms: ['Mobile'],
+			},
+			{
+				images: ['fakeLink'],
+				title: 'Game 2',
+				price: 55,
+				id: '2',
+				genres: ['Action', 'Open World'],
+				platforms: ['PC'],
+			},
+		];
+
+		render(
+			<GamesContainer
+				games={games}
+				isGamesLoading={false}
+				gamesError={false}
+				getNewGames={getNewGamesMock}
+				addToCart={vi.fn()}
+				refetchGames={vi.fn()}
+			/>
+		);
+
+		// Ensure button is eventually shown
+		await waitFor(
+			async () => {
+				const showMoreButton = screen.getByText('Show More');
+				await user.click(showMoreButton);
+			},
+			{ timeout: 5000 }
+		);
+
+		// Verify getNewGamesMock was called
+		expect(getNewGamesMock).toHaveBeenCalled();
 	});
 });
