187f5688fd13ec2c674aae0784a48857e31e2bbe
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-02-19 19:18:10 +0800

    feat: Ensure top bar is always on top of the layout

--- src/TopBar.jsx
+++ src/TopBar.jsx
@@ -4,15 +4,18 @@ import { Link } from 'react-router-dom';
 import Search from './Search.jsx';
 import styled from 'styled-components';
 
+const breakPoint = 932;
+
 const Container = styled.div`
-	/* box-sizing: border-box;
-	margin: 0px;
+	grid-column: 2 / 3;
+	grid-row: 1 / 2;
 
 	display: flex;
 	justify-content: space-between;
-	gap: 10px;
-	background-color: #1b1e22;
-	padding: 20px 30px; */
+	@media (max-width: ${breakPoint}px) {
+		grid-row: 1/ 2;
+		grid-template-columns: -1 / 1;
+	}
 `;
 
 const Actions = styled.div`
