3b9acae01bec7a2fc45a38e46a64fc01dad5e43e
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-02-22 13:34:27 +0800

    feat: Add initial styling to games contaienr mainly on positioning the buttons on the bottom with position absolute

--- src/GameDetails.jsx
+++ src/GameDetails.jsx
@@ -6,8 +6,45 @@ import Image from './Image.jsx';
 import styled from 'styled-components';
 import { useEffect, useState } from 'react';
 
-const Teaser = styled.div`
-	/* width: fit-content; */
+const Teaser = styled.div``;
+
+const Container = styled.div`
+	height: 100%;
+	overflow: scroll;
+`;
+
+const Actions = styled.div`
+	display: flex;
+
+	& button,
+	a,
+	div {
+		width: 100%;
+		height: 100%;
+		border-radius: 0.5em;
+		font-size: 0.9em;
+		padding: 0.25em;
+		text-align: center;
+	}
+
+	& a {
+		background-color: transparent;
+		border: 1px solid white;
+		display: flex;
+		justify-content: center;
+		align-content: center;
+	}
+
+	@media (max-width: 600px) {
+		bottom: 54px;
+		width: 100%;
+		position: fixed;
+		background-color: #453e3e;
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		padding: 0.5em;
+		gap: 0.5em;
+	}
 `;
 
 function GameDetails({ game, isLoading, error, onAddItemToCart, refetchGame }) {
@@ -21,7 +58,7 @@ function GameDetails({ game, isLoading, error, onAddItemToCart, refetchGame }) {
 	}, [error]);
 
 	return (
-		<>
+		<Container>
 			{isLoading ? (
 				<div title="loading-indicator" className="loadingIndicator">
 					Loading...
@@ -62,15 +99,18 @@ function GameDetails({ game, isLoading, error, onAddItemToCart, refetchGame }) {
 						<div className="descriptionContent">{game.description ? game.description : 'No available description.'}</div>
 					</div>
 					<div className="price">{game.price ? `$${game.price.toFixed(2)}` : 'Price Unavailable'}</div>
-					<AddToCartButton
-						isGameAdded={game.isAddedToCart}
-						onAddItemToCart={() => {
-							onAddItemToCart(game);
-						}}
-					/>
-					<a href="https://store.steampowered.com/">
-						<button>Buy Now</button>
-					</a>
+					<Actions>
+						<AddToCartButton
+							isGameAdded={game.isAddedToCart}
+							onAddItemToCart={() => {
+								onAddItemToCart(game);
+							}}
+							caption={'Add to Cart'}
+						/>
+						<a href="https://store.steampowered.com/">
+							<div className="buyNowButton">Buy Now</div>
+						</a>
+					</Actions>
 					<div className="otherDetails">
 						<div className="genre">
 							<h2 className="genreHeading">Genre</h2>
@@ -101,7 +141,7 @@ function GameDetails({ game, isLoading, error, onAddItemToCart, refetchGame }) {
 					</div>
 				</div>
 			)}
-		</>
+		</Container>
 	);
 }
 
